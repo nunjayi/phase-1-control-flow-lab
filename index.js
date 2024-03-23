7bbd6e22c0512b08331ee64fe72ccf384b9f4526
nunjayi <epenyash@gmail.com>
2024-03-23 20:10:49 +0300

    adding switch, ternary and if and else statement to the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    if(feet<=400){
+      return 'This one is on me!'
+    }else if(feet<2000){
+      return 'That will be twenty bucks.'
+    }else if(feet<2500){
+      return 'I will gladly take your thirty bucks.'
+    }else if(feet>2500){
+      return 'No can do.'
+
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    const result  = city === 'NYC'? 'Ok, sounds good.': 'No go.'
+    return result
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(isGenerous){
   // Write your code here!
+  switch(isGenerous){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+
+  }
 }
